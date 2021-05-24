b5801e4569d7f2e4954544cb815dd2f13dcdb820
HanaIroha <61019733+HanaIroha@users.noreply.github.com>
2021-05-25 00:29:31 +0700

    asd

---
+++ QuanLyThuVien/QuanLyThuVien/BUS/ChiTietPhieuMuonBUS.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using QuanLyThuVien.DAL;
+
+namespace QuanLyThuVien.BUS
+{
+    class ChiTietPhieuMuonBUS
+    {
+        DataConnection dataConnect = new DataConnection();
+        public DataTable GetPhieuMuon()
+        {
+            string sql = "select * from dbo.CHITIETPHIEUMUON";
+            DataTable dt = new DataTable();
+            dt = dataConnect.GetTable(sql);
+            return dt;
+        }
+        public void SuaTinhTrang(int ID, int TinhTrang)
+        {
+            string sql = "update dbo.CHITIETPHIEUMUON SET TinhTrang = '" + TinhTrang + "' where ID = '" + ID + "'";
+            dataConnect.ExecuteNonQuery(sql);
+        }
+        public void SuaTinhTrang(int ID, DateTime ngayTra)
+        {
+            string sql = "update dbo.CHITIETPHIEUMUON SET NgayTra = '" + ngayTra + "' where ID = '" + ID + "'";
+            dataConnect.ExecuteNonQuery(sql);
+        }
+    }
+}
---
+++ QuanLyThuVien/QuanLyThuVien/BUS/PhieuMuonBUS.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuanLyThuVien.DAL;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace QuanLyThuVien.BUS
+{
+    class PhieuMuonBUS
+    {
+        DataConnection dataConnect = new DataConnection();
+        public DataTable GetPhieuMuon()
+        {
+            string sql = "select * from dbo.PHIEUMUON";
+            DataTable dt = new DataTable();
+            dt = dataConnect.GetTable(sql);
+            return dt;
+        }
+
+        public void SuaTen(int SoPhieuMuon, string tenDangNhap)
+        {
+            string sql = "update dbo.PHIEUMUON SET TenDangNhap = '" + tenDangNhap + "' where SoPhieuMuon = '" + SoPhieuMuon + "'";
+            dataConnect.ExecuteNonQuery(sql);
+        }
+        public void SuaMaSV(int SoPhieuMuon, string MaSV)
+        {
+            string sql = "update dbo.PHIEUMUON SET MaSinhVien = '" + MaSV + "' where SoPhieuMuon = '" + SoPhieuMuon + "'";
+            dataConnect.ExecuteNonQuery(sql);
+        }
+    }
+}
